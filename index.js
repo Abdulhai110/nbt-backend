54ecff4e2f40ec2a4094ed54ef02d483b8fc4462
Abdul Hai Khan <abdul.hai@codegenio.com>
2026-06-16 01:16:37 +0500

    build issue fix

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ const app = require('./src/app');
 let isConnected = false;
 
 module.exports = async (req, res) => {
-  if (!isConnected) {
-    await connectDB();
-    isConnected = true;
+  try {
+    if (!isConnected) {
+      await connectDB();
+      isConnected = true;
+    }
+    app(req, res);
+  } catch (err) {
+    console.error('CRASH:', err); // This will show in Vercel logs
+    res.status(500).json({ error: err.message }); // Shows error in browser too
   }
-  app(req, res);
 };
